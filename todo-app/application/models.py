d0bad822f4640e8890aa9f74bc2b32255af9b273
agray998 <agray@qa.com>
2022-12-12 14:16:22 +0000

    added database integration

--- todo-app/application/models.py
+++ todo-app/application/models.py
@@ -1,5 +1,11 @@
 from application import app, db
 
+class Users(db.Model):
+    user_id = db.Column(db.Integer, primary_key = True)
+    forename = db.Column(db.String(20))
+    surname = db.Column(db.String(20))
+    tasks = db.relationship('Tasks', backref="users") # user.tasks holds list of tasks associated to user, also creates backref to access a user from their tasks
+
 class Tasks(db.Model): # tasks table
     # __tablename__ = "todo_items" - if changing table name
     # need: due date, priority level, name, description
@@ -8,5 +14,6 @@ class Tasks(db.Model): # tasks table
     description = db.Column(db.String(100))
     priority = db.Column(db.Integer)
     due = db.Column(db.String(10))
+    user = db.Column(db.Integer, db.ForeignKey('users.user_id')) # creates foreign key constraint
     def __str__(self):
-        return f"{self.name}: {self.description}, due {self.due}. Priority {self.priority}."
+        return f"{self.name}: {self.description}, due {self.due}. Priority {self.priority}. Assignee: {self.users.forename} {self.users.surname}"
--- todo-app/application/routes.py
+++ todo-app/application/routes.py
@@ -1,19 +1,60 @@
 from application import app, db
-from flask import request
-from application.models import Tasks
+from flask import request, redirect, url_for
+from application.models import Tasks, Users
 
 @app.route("/")
 def index():
     return "Todo List!"
 
+# DB operations
+# (c)reate /
+# (r)ead /
+# (u)pdate /
+# (d)elete /
+
+@app.route("/add-user")
+def add_user():
+    forename, surname = request.args.get("forename"), request.args.get("surname")
+    user = Users(forename = forename, surname = surname)
+    db.session.add(user)
+    db.session.commit()
+    return "added user"
+
 @app.route("/add")
 def add_todo():
+    user = int(request.args.get("user"))
     name = request.args.get("name")
     desc = request.args.get("desc")
     priority = int(request.args.get("priority"))
     due = request.args.get("due")
-    task = Tasks(name=name, description=desc, priority=priority, due=due)
+    task = Tasks(name=name, description=desc, priority=priority, due=due, user = user)
     db.session.add(task)
     db.session.commit()
     return f"Added {str(task)}"
 
+@app.route("/view-tasks")
+def view_tasks():
+    tasks = Tasks.query.order_by(Tasks.priority).all() # can user filter_by to narrow results, eg: filter_by(name="sample 2")
+    return '<br>'.join(map(str, tasks))
+
+@app.route("/task/<int:id>")
+def get_task(id):
+    return str(Tasks.query.get(id))
+
+@app.route("/update/<int:id>")
+def update_task(id):
+    task = Tasks.query.get(id)
+    task.name = request.args.get("name", task.name)
+    task.description = request.args.get("desc", task.description)
+    task.priority = int(request.args.get("priority", task.priority))
+    task.due = request.args.get("due", task.due)
+    task.user = int(request.args.get("user", task.user))
+    db.session.commit()
+    return redirect(url_for('view_tasks'))
+
+@app.route("/delete-task/<int:id>")
+def delete_task(id):
+    task = Tasks.query.get(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('view_tasks'))
